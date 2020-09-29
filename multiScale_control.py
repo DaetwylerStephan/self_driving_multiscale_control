8d354b8bbbbc78d7cfb5ad74d61dca99d4c9dbb9
DaetwylerStephan <Stephan.Daetwyler@UTSouthwestern.edu>
2020-09-28 22:11:05 -0500

    add log to gitignore

--- multiScale_control.py
+++ multiScale_control.py
@@ -7,16 +7,17 @@ Core python script to start the multiScale control
 import time
 import logging
 
+#learn more about logging: https://realpython.com/python-logging/
 #generate filename for log file
 timestr = time.strftime("%Y%m%d-%H%M%S")
 logging_filename = timestr + '.log'
 
 #set configuration of log file to be of log file, with the given name, all logging levels and format of reporting
 logging.basicConfig(filename='log/'+logging_filename, level=logging.INFO, format='%(asctime)-8s:%(levelname)s:%(threadName)s:%(thread)d:%(module)s:%(name)s:%(message)s')
+
+#set up custom logger
 logger = logging.getLogger(__name__)
 logger.info('multiScale microscope started')
-
-print(timestr)
 
 def main():
     """
