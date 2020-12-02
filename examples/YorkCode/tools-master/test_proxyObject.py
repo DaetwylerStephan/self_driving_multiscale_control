ac5e9d6d42f1af7f1afec168fe6704d44750f34a
DaetwylerStephan <Stephan.Daetwyler@UTSouthwestern.edu>
2020-12-02 15:01:19 -0600

    update yorkcode example

--- examples/YorkCode/tools-master/test_proxyObject.py
+++ examples/YorkCode/tools-master/test_proxyObject.py
@@ -39,18 +39,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-            #####################################################
-            #  This code is imported by the example code above. #
-            #       Copypaste it into 'dummy_module.py'         #
-            #####################################################
-class Camera:
-    def record(self, out):
-        out.fill(1)
-
-class Preprocessor:
-    def process(self, x, out):
-        x.max(axis=0, out=out)
-
-class Display:
-    def show(self, image):
-        pass
