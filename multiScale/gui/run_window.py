594479c2effefbdfb692ea516d5b173406360cac
DaetwylerStephan <Stephan.Daetwyler@UTSouthwestern.edu>
2021-01-26 21:57:04 -0600

    work on the MVC run stack function

--- multiScale/gui/run_window.py
+++ multiScale/gui/run_window.py
@@ -26,22 +26,23 @@ class Run_Tab(tk.Frame):
         #set the different label frames
         preview_settings = tk.LabelFrame(self, text="Preview")
         stack_aquisition_settings = tk.LabelFrame(self, text="Stack acquisition")
-        timelapse_acquisition_settings = tk.LabelFrame(self, text="Time-lapse aquisition")
+        timelapse_acquisition_settings = tk.LabelFrame(self, text="Time-lapse acquisition")
 
         # overall positioning of label frames
-        preview_settings.grid(row=0, column=1)
+        preview_settings.grid(row=0, column=1, sticky = tk.W + tk.E)
         stack_aquisition_settings.grid(row=1, column=1, sticky = tk.W + tk.E)
-        timelapse_acquisition_settings.grid(row=1, column=1, sticky=tk.W + tk.E)
+        timelapse_acquisition_settings.grid(row=2, column=1, sticky=tk.W + tk.E)
 
         ### ----------------------------preview buttons ---------------------------------------------------------------
         #preview settings-----------------------------------------------------------------------------------
-        self.bt_changeTo488 = tk.Button(preview_settings, text="488 nm", command=self.preview_changeTo488, bg="#00f7ff")
-        self.bt_changeTo552 = tk.Button(preview_settings, text="552 nm", command=self.preview_changeTo552, bg="#a9ff00")
-        self.bt_changeTo594 = tk.Button(preview_settings, text="594 nm", command=self.preview_changeTo594, bg="#ffd200")
-        self.bt_changeTo640 = tk.Button(preview_settings, text="640 nm", command=self.preview_changeTo640, bg="#ff2100")
-        self.bt_changeTo_block = tk.Button(preview_settings, text="no filter", command=self.preview_changeToBlock)
-        self.bt_changeTo_trans = tk.Button(preview_settings, text="block", command=self.preview_changeToTransmission)
-        self.bt_preview = tk.Button(preview_settings, text="Preview", command=self.preview)
+        self.bt_changeTo488 = tk.Button(preview_settings, text="488 nm", command= lambda : self.preview_filter_select(self.bt_changeTo488), bg="#00f7ff")
+        self.bt_changeTo552 = tk.Button(preview_settings, text="552 nm", command= lambda : self.preview_filter_select(self.bt_changeTo552), bg="#a9ff00")
+        self.bt_changeTo594 = tk.Button(preview_settings, text="594 nm", command=lambda : self.preview_filter_select(self.bt_changeTo594), bg="#ffd200")
+        self.bt_changeTo640 = tk.Button(preview_settings, text="640 nm", command=lambda : self.preview_filter_select(self.bt_changeTo640), bg="#ff2100")
+        self.bt_changeTo_block = tk.Button(preview_settings, text="no filter", command=lambda : self.preview_filter_select(self.bt_changeTo_block))
+        self.bt_changeTo_trans = tk.Button(preview_settings, text="block", command=lambda : self.preview_filter_select(self.bt_changeTo_trans))
+        self.bt_preview_lowres = tk.Button(preview_settings, text="Low Res Preview", command= lambda : self.preview_change(self.bt_preview_lowres))
+        self.bt_preview_highres = tk.Button(preview_settings, text="High Res Preview", command=lambda : self.preview_change(self.bt_preview_highres))
 
         #preview layout
         self.bt_changeTo488.grid(row=3, column=2)
@@ -50,22 +51,41 @@ class Run_Tab(tk.Frame):
         self.bt_changeTo640.grid(row=3, column=5)
         self.bt_changeTo_block.grid(row=3, column=6)
         self.bt_changeTo_trans.grid(row=3, column=7)
-        self.bt_preview.grid(row=4, column=2, columnspan=2, sticky = (tk.W + tk.E))
+        self.bt_preview_lowres.grid(row=4, column=2, columnspan=2, sticky = (tk.W + tk.E))
+        self.bt_preview_highres.grid(row=4, column=4, columnspan=2, sticky=(tk.W + tk.E))
 
         ### ----------------------------stack acquisition buttons ------------------------------------------------------
         #stack aquisition labels (positioned)
+        laseron_label = ttk.Label(stack_aquisition_settings, text="Laser On:").grid(row=2, column=0)
         numberOfPlanes_label= ttk.Label(stack_aquisition_settings, text="Number of planes:").grid(row = 3, column = 0)
+        plane_spacing_label= ttk.Label(stack_aquisition_settings, text="Spacing of planes:").grid(row = 4, column = 0)
 
         #stack aquisition settings
         self.numberOfPlanes = tk.IntVar()
         self.Entry_numberOfPlanes = tk.Entry(stack_aquisition_settings, textvariable = self.numberOfPlanes)
         self.Entry_numberOfPlanes.insert(0,"20")
 
+        self.plane_spacing = tk.DoubleVar()
+        self.Entry_plane_spacing = tk.Entry(stack_aquisition_settings, textvariable=self.plane_spacing)
+        self.Entry_plane_spacing.insert(0, "1")
+
+        self.bt_run_lowresstack = tk.Button(stack_aquisition_settings, text="Acquire Low Res Stack(s)")
+        self.bt_run_highresstack = tk.Button(stack_aquisition_settings, text="Acquire High Res Stack(s)")
+
+        self.bt_laserOn488 = tk.Checkbutton(stack_aquisition_settings, text ='488')
+        self.bt_laserOn552 = tk.Checkbutton(stack_aquisition_settings, text ='552')
+        self.bt_laserOn594 = tk.Checkbutton(stack_aquisition_settings, text ='594')
+        self.bt_laserOn640 = tk.Checkbutton(stack_aquisition_settings, text ='640')
+
         #stack aquisition layout (labels positioned above)
-        self.Entry_numberOfPlanes.grid(row =3, column=1)
-
-
-
+        self.bt_laserOn488.grid(row =2, column=1)
+        self.bt_laserOn552.grid(row=2, column=2)
+        self.bt_laserOn594.grid(row=2, column=3)
+        self.bt_laserOn640.grid(row=2, column=4)
+        self.Entry_numberOfPlanes.grid(row =3, column=1, columnspan=3, sticky = tk.W + tk.E)
+        self.Entry_plane_spacing.grid(row =4, column=1, columnspan=3, sticky = tk.W + tk.E)
+        self.bt_run_lowresstack.grid(row = 5, column =0, columnspan=3, sticky = tk.W + tk.E)
+        self.bt_run_highresstack.grid(row=5, column=3, columnspan=3, sticky=tk.W + tk.E)
 
         ch_button = ttk.Button(self, text="Change", command=self.loop_function)
         ch_button.grid(row=0, column=3, sticky=tk.E)
@@ -94,43 +114,23 @@ class Run_Tab(tk.Frame):
 
 
 
-#-------preview functions---------------------------------------------------------------------------------------------
+#-------button press functions---------------------------------------------------------------------------------------------
 #---------------------------------------------------------------------------------------------------------------------
 
-    def preview(self):
-        if (self.bt_preview.cget('relief') == "sunken"):
-            self.bt_preview.config(relief="raised")
+    def preview_change(self, button):
+        if (button.cget('relief') == "sunken"):
+            button.config(relief="raised")
         else:
-            self.bt_preview.config(relief="sunken")
+            button.config(relief="sunken")
 
-
-    def preview_changeTo488(self):
-        self.changeFilter('515-30-25')
-        self.switchLaser('488')
-
-    def preview_changeTo552(self):
-        self.changeFilter('572/20-25')
-        self.switchLaser('552')
-
-    def preview_changeTo594(self):
-        self.changeFilter('615/20-25')
-        self.switchLaser('594')
-
-    def preview_changeTo640(self):
-        self.changeFilter('676/37-25')
-        self.switchLaser('640')
-
-    def preview_changeToTransmission(self):
-        self.changeFilter('transmission')
-
-    def preview_changeToBlock(self):
-        self.changeFilter('block')
-
-    def changeFilter(self, filter):
-        pass
-
-    def switchLaser(self, laserline):
-        pass
+    def preview_filter_select(self, button):
+        self.bt_changeTo488.config(relief="raised")
+        self.bt_changeTo552.config(relief="raised")
+        self.bt_changeTo594.config(relief="raised")
+        self.bt_changeTo640.config(relief="raised")
+        self.bt_changeTo_block.config(relief="raised")
+        self.bt_changeTo_trans.config(relief="raised")
+        button.config(relief="sunken")
 
 #---------------------------------------------------------------------------------------------------------------------
 #---------------------------------------------------------------------------------------------------------------------
--- multiScale/multiScale_main.py
+++ multiScale/multiScale_main.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 from tkinter import ttk
 
+import time
+
 from gui.main_window import MultiScope_MainGui
 from multiScope import multiScopeModel
 
@@ -20,22 +22,58 @@ class MultiScale_Microscope_Controller():
         self.view = MultiScope_MainGui(all_tabs_mainGUI, self.model)
 
         #define here which buttons run which function in the multiScope model
-        self.view.runtab.bt_preview.bind("<Button>", self.run_preview)
 
+        #buttons run tab
+        self.view.runtab.bt_preview_lowres.bind("<Button>", self.run_lowrespreview)
+        self.view.runtab.bt_preview_highres.bind("<Button>", self.run_highrespreview)
+        self.view.runtab.bt_changeTo488.bind("<Button>", lambda event: self.changefilter(event, '488', '488_filter'))
+        self.view.runtab.bt_changeTo552.bind("<Button>", lambda event: self.changefilter(event, '552', '552_filter'))
+        self.view.runtab.bt_changeTo594.bind("<Button>", lambda event: self.changefilter(event, '594', '594_filter'))
+        self.view.runtab.bt_changeTo640.bind("<Button>", lambda event: self.changefilter(event, '640', '640_filter'))
+        self.view.runtab.bt_changeTo_block.bind("<Button>", lambda event: self.changefilter(event, 'None', 'Block'))
+        self.view.runtab.bt_changeTo_trans.bind("<Button>", lambda event: self.changefilter(event, 'LED', 'No_filter'))
+        self.view.runtab.bt_run_lowresstack.bind("<Button>", self.acquire_lowresstack)
+        self.view.runtab.bt_run_highresstack.bind("<Button>", self.acquire_highresstack)
 
 
     def run(self):
         self.root.title("Multi-scale microscope V1")
         self.root.geometry("800x600")
-        self.resizable(width=False, height=False)
+        self.root.resizable(width=False, height=False)
         self.root.mainloop()
 
     def close(self):
         self.model.close()
 
-    def run_preview(self, event):
+    def run_lowrespreview(self, event):
         print("running preview")
         self.model.lowres_camera.take_snapshot(20)
+
+    def run_highrespreview(self, event):
+        print("running preview")
+        self.model.lowres_camera.take_snapshot(20)
+
+    def changefilter(self, event, laser, filter):
+        print("filter " + filter)
+        print("laser " + laser)
+
+    def acquire_lowresstack(self, event):
+        self.view.runtab.bt_run_lowresstack.config(relief="sunken")
+        print("acquiring low res stack")
+        print("number of planes: " + str(self.view.runtab.numberOfPlanes.get()) + ", plane spacing: " + str(self.view.runtab.plane_spacing.get()))
+        self.view.runtab.bt_run_lowresstack.config(relief="raised")
+
+    def acquire_highresstack(self, event):
+
+        self.view.runtab.bt_run_highresstack.config(relief="sunken")
+        self.view.update()
+
+        time.sleep(2)
+        print("acquiring high res stack")
+        print("number of planes: " + str(self.view.runtab.numberOfPlanes.get()) + ", plane spacing: " + str(self.view.runtab.plane_spacing.get()))
+
+        self.view.runtab.bt_run_highresstack.config(relief="raised")
+
 
 if __name__ == '__main__':
     c = MultiScale_Microscope_Controller()
